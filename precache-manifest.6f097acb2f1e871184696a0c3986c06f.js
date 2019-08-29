057cd26b9feb1316ffad0ff5c71dcc393d401e7a
EvgeniyPashuk <mess1ah.cfg@gmail.com>
2019-08-29 11:19:03 +0300

    Updates

---
+++ precache-manifest.6f097acb2f1e871184696a0c3986c06f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c36fe5e9b91663576c82e20e2a12158b",
+    "url": "/cart-project/index.html"
+  },
+  {
+    "revision": "fbbfca9155b75ce626d6",
+    "url": "/cart-project/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9f9a68cd7112f1b603f8",
+    "url": "/cart-project/static/js/2.3824815e.chunk.js"
+  },
+  {
+    "revision": "fbbfca9155b75ce626d6",
+    "url": "/cart-project/static/js/main.84d3fc26.chunk.js"
+  },
+  {
+    "revision": "e1e215fa4b36b9aaf708",
+    "url": "/cart-project/static/js/runtime~main.17058b18.js"
+  }
+]);
